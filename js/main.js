dc415e46db6fd2f07578478c1efeaa9844b6d38c
Andrew Girgis Sobhi <138126599+AndrewGirgis099@users.noreply.github.com>
2024-09-03 00:14:02 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,32 +1,32 @@
-// document.addEventListener("DOMContentLoaded", function() {
-//     const scrollToTopBtn = document.getElementById("scrollToTopBtn");
-//     const whatsappBtn = document.getElementById("whatsappBtn");
+document.addEventListener("DOMContentLoaded", function() {
+    const scrollToTopBtn = document.getElementById("scrollToTopBtn");
+    const whatsappBtn = document.getElementById("whatsappBtn");
 
-//     function checkScroll() {
-//         if (document.body.scrollTop > window.innerHeight || document.documentElement.scrollTop > window.innerHeight) {
-//             scrollToTopBtn.style.display = "block";
-//             whatsappBtn.style.display = "block";
-//         } else {
-//             scrollToTopBtn.style.display = "none";
-//             whatsappBtn.style.display = "none";
-//         }
-//     }
+    function checkScroll() {
+        if (document.body.scrollTop > window.innerHeight || document.documentElement.scrollTop > window.innerHeight) {
+            scrollToTopBtn.style.display = "block";
+            whatsappBtn.style.display = "block";
+        } else {
+            scrollToTopBtn.style.display = "none";
+            whatsappBtn.style.display = "none";
+        }
+    }
 
-//     let isScrolling; // Variable to track the scroll state
+    let isScrolling; // Variable to track the scroll state
 
-//     window.onscroll = function() {
-//         if (isScrolling) {
-//             cancelAnimationFrame(isScrolling);
-//         }
+    window.onscroll = function() {
+        if (isScrolling) {
+            cancelAnimationFrame(isScrolling);
+        }
 
-//         isScrolling = requestAnimationFrame(checkScroll);
-//     };
+        isScrolling = requestAnimationFrame(checkScroll);
+    };
 
-//     // Scroll to the top of the document when the user clicks on the scroll-to-top button
-//     scrollToTopBtn.addEventListener("click", function() {
-//         window.scrollTo({
-//             top: 0,
-//             behavior: "smooth" // For smooth scrolling
-//         });
-//     });
-// });
+    // Scroll to the top of the document when the user clicks on the scroll-to-top button
+    scrollToTopBtn.addEventListener("click", function() {
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth" // For smooth scrolling
+        });
+    });
+});
